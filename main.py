4ef0b0570bc080d6b1db704d0ee41ad39ac7c1f1
hehez <yanhongfei2012@gmail.com>
2019-10-02 19:59:56 -0400

    update pdf parser

--- main.py
+++ main.py
@@ -1,29 +1,48 @@
-from subprocess import call
 import re
 import csv
 import sys
 
-if len(sys.argv) != 2:
-	print "Format: "+sys.argv[0]+" [PDF file]"
-	sys.exit()
+from subprocess import call
 
-call(["pdf2txt.py", "-t", "tag", "-o", "output", sys.argv[1]])
-
-raw = open("output").read()
-
-searchObj = re.findall(r'Total for(\d\d\d \d\d\d-\d\d\d\d)(\d*.\d*) ', raw)
-total=0
+billing_cycle_date = re.compile(r'(\d{,2}/\d{,2}/\d{,2}-\d{,2}/\d{,2}/\d{,2})')
+billing_cycle_date_new = re.compile(r'Issue Date:([A-z]{3}|[A-z]{19})\s+(\d{1,}), (\d{2,4})')
+indivial_cell_fee = re.compile(r'Total for(\d\d\d \d\d\d-\d\d\d\d)(\d*\.\d*)(CR| )')
+indivial_cell_fee_new = re.compile(r'Total for\s*(\d\d\d.\d\d\d.\d\d\d\d)(\$\d*\.\d*)')
+indivial_cell = re.compile(r'(\d\d\d \d\d\d-\d\d\d\d)')
+data_fee = re.compile(r'1\.Mobile Share Value \d+GB with Rollover Data(\d+\.\d*)')
 
 
-with open('RawData.csv','w') as output:
-	writer = csv.writer(output)
-	if searchObj:
-		# print searchObj.group()
-		for item in searchObj:
-			phone = item[0][0]+item[0][1]+item[0][2]+item[0][4]+item[0][5]+item[0][6]+item[0][8]+item[0][9]+item[0][10]+item[0][11]			#print phone+": "+item[1]
-			writer.writerow((item[0],phone,item[1]))
-			total += float(item[1])
-	else:
-		print "No match!"
-	#print "Total: " + str(total)
-	writer.writerow(('Total',str(total)))
+def ParseBillPDF():
+    call(["pdf2txt.py", "-t", "tag", "-o", "output", sys.argv[1]])
+    f = open("output")
+    raw = f.read()
+    searchObj = indivial_cell_fee.findall(raw)
+    d = billing_cycle_date.findall(raw)
+    date = next(iter(set(d)))
+    data = data_fee.findall(raw)
+    f.close()
+
+    with open('billing_cycle_date.csv', 'w') as bcd:
+        writer =  csv.writer(bcd)
+        writer.writerow([date])
+
+    with open('RawData.csv','w') as wf:
+        writer = csv.writer(wf)
+        if searchObj:
+            for item in searchObj:
+                print item
+				phone = item[0].replace('-', '').replace(' ','')
+                writer.writerow((item[0], phone, item[1], item[2]))
+        else:
+            print "No match!"
+
+
+def main():
+    if len(sys.argv) != 2:
+        print "Format: " + sys.argv[0] + " [PDF file]"
+        sys.exit()
+    ParseBillPDF()
+
+
+if __name__ == '__main__':
+    main()
